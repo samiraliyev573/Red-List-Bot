dffa35a2be6fda0ea04052e21218fecf6572870e
Heroforfun <aliyev7@mail.ru>
2020-10-29 19:38:45 -0700

    More patterns and responses
    Intents.json updated

--- src/nltkproperties.py
+++ src/nltkproperties.py
@@ -21,6 +21,7 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
+    # stem every word in the given sentence
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
     bag = np.zeros(len(all_words), dtype=np.float32)
     for index, word in enumerate(all_words):
